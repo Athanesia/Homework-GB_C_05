cd38d85e26070d5d124aff24ce3b6b9a92c4c28b
Anna <zhigalovanna.28@gmail.com>
2022-12-07 22:15:14 +0300

    Add Sum even

---
+++ Task_36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Random rand = new Random();
+int[] array = new int[4]; 
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = rand.Next(-9,99);
+}
+
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+
+void SumEvenIndex(int[] array)
+{
+    int sumEven = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sumEven = array[i] + sumEven;
+    }
+    Console.WriteLine($"Cуммa элементов, стоящих на нечётных позициях: {sumEven}");
+}
+
+Console.WriteLine();
+SumEvenIndex(array);
+
+
