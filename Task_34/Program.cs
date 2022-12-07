7dc5866e106be69428684ddc984de728a7a7b909
Anna <zhigalovanna.28@gmail.com>
2022-12-07 22:02:07 +0300

    Add Random massive even

---
+++ Task_34/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Random rand = new Random(); 
+int[] array = new int[4]; 
+for (int i = 0; i < array.Length; i++) 
+{ 
+array[i] = rand.Next(100,999); 
+} 
+ 
+for(int i = 0; i < array.Length; i++) 
+{ 
+Console.Write(array[i] + " "); 
+} 
+ 
+int EvenCountInt(int[] array) 
+{ 
+    int count = 0; 
+    foreach (int i in array) 
+    { 
+        if (i % 2 == 0) count++; 
+    } 
+    return count; 
+} 
+ 
+Console.WriteLine(); 
+Console.WriteLine($"Количество чётных чисел = {EvenCountInt(array)}");
